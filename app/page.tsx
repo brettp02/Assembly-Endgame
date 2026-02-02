e3a46bea28d59c2cb0a1abf02607b766cba43fc2
Brett <brettpenwarden@gmail.com>
2026-02-03 12:04:21 +1300

    created status bar draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { ComponentExample } from "@/components/component-example";
 import Header from "@/components/Header";
+import Status from "@/components/Status";
 
 export default function Page() {
     return (
-        <div className="flex flex-col items-center min-h-screen max-w-3xl py-12 px-4 mx-auto">
+        <main className="flex flex-col items-center min-h-screen max-w-3xl py-12 px-4 mx-auto">
             <Header />
-        </div>
+            <Status />
+        </main>
     )
 }
---
+++ components/Status.tsx
@@ -0,0 +1,8 @@
+export default function Status() {
+    return (
+        <section className={'mt-8 bg-green-700/90 w-xl h-[10vh] rounded-xl flex flex-col items-center justify-center shadow-2xl gap-2 text-center'}>
+            <h2 className={'text-2xl'}>You win!</h2>
+            <p className={'text-xl text-foreground font-extralight'}>Well done!🎉</p>
+        </section>
+    )
+}
